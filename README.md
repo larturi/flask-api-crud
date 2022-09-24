8000d5fb699e314f066748b45c6480b5bc6b7315
Leandro Arturi <lea.arturi@gmail.com>
2022-09-24 16:08:27 -0300

    Finalizado

--- README.md
+++ README.md
@@ -2,7 +2,9 @@
 
 1. Clonar proyecto
 
-2. Instalar dependencias
+2. Crear el .env a partir de .env.template
+
+3. Instalar dependencias
 
 ```bash
 pipenv shell
@@ -10,23 +12,25 @@ pipenv shell
 pipenv install
 ```
 
-3. Levantar la base de datos Postgres
+4. Levantar la base de datos Postgres
 
 ```bash
 docker-compose up -d
 ```
 
-4. Crear tablas Postgres
+5. Crear tablas Postgres
 
 ```bash
 http://127.0.0.1:5000/api/seed
 ```
 
-5. Ejecutar el servidor
+6. Ejecutar el servidor
 
 ```bash
 python app.py
 ```
+
+Endpoint: <http://127.0.0.1:5000/api/users>
 
  ---
 
--- app.py
+++ app.py
@@ -1,19 +1,26 @@
 from flask import Flask, jsonify, request
 from psycopg2 import connect, extras
 from cryptography.fernet import Fernet
+from os import environ
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+key = Fernet.generate_key()
+
+host = environ.get('POSTGRES_HOST')
+database = environ.get('POSTGRES_DB')
+username = environ.get('POSTGRES_USER')
+password = environ.get('POSTGRES_PASSWORD')
+port = environ.get('DB_PORT')
 
 app = Flask(__name__)
 
 key = Fernet.generate_key()
 
-host = "localhost"
-port = "5432"
-dbname = "flask-crud"
-user = "flask"
-password = "flask"
-
 def db_connection():
-    conn = connect(host=host, port=port, dbname=dbname, user=user,password=password)
+    conn = connect(host=host, port=port, dbname=database, user=username,password=password)
     return conn
 
 @app.get('/api/seed')
